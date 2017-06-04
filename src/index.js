73ffb589bc49bff1832380d8101706b2732aac07
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-06-04 19:25:38 +0400

    Initialized project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ class Hello extends React.Component {
+
+ 	render() { return <div>Hello!</div>}
+ }
+
+  class Goodbye extends React.Component {
+
+ 	render() { return <div>Goodbye!</div>}
+ }
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
