6c458e537217cc01440198e25ad2751a277310d4
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-06-06 18:20:22 +0400

    Created action and reducer for fetching posts

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import reducers from './reducers';
 
 import PostsIndex from './components/posts_index';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 
